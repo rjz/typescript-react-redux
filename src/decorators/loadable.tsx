b841a3dfffc1741e95530f34515f30b61b1dcaca
rjz <rj@rjzaworski.com>
2017-09-04 17:48:14 -0700

    Clarifies higher-order component

--- src/decorators/loadable.tsx
+++ src/decorators/loadable.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 
+// `loadable` is curried to decouple an `isLoading` check from the details of
+// component implementation
 export default function loadable<P>(isLoading: (p: P) => boolean) {
+  // Return a higher-order component implementing the "loadable" behavior
+  // See: https://goo.gl/TxPPCw
   return (C: React.ComponentClass<P>|React.SFC<P>): React.SFC<P> => {
     const LoadableComponent: React.SFC<P> = (props) => {
       if (isLoading(props)) {
